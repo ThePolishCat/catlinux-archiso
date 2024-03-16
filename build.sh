36d9a70184e2b073a5354eda375474477d345a08
none <none>
2024-03-16 23:23:04 +0100

    zmieniono:       build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 echo "installing dependences"
 sudo pacman -S gum archiso
 mkdir work
-mkarchiso -v -w work archiso
+sudo mkarchiso -v -w work archiso
 gum confirm "Delete work folder?" && sudo rm -fr work
 
