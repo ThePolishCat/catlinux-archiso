0ab012ac39bb1d615d1e78db909556bafee1204f
none <none>
2024-03-18 00:17:41 +0000

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "installing dependences"
-sudo pacman -S gum archiso
+sudo pacman -S gum archiso --needed
 mkdir work
 sudo mkarchiso -v -w work archiso
 gum confirm "Delete work folder?" && sudo rm -fr work
